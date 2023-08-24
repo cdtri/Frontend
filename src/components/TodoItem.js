cc3d07b55c9bffd49f1b8701d68e1066713081d5
Tri Cao <cdtri1619lqdnt@gmail.com>
2023-08-24 17:26:23 +0700

    Thêm checkbox cho TodoItem Component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,9 @@ import React from "react";
 class TodoItem extends React.Component {
     render() {
         return (
-            <li className="todo-item" key={this.props.todo.id}>{this.props.todo.title}</li>
+            <li className="todo-item" key={this.props.todo.id}>
+                <input type="checkbox" />{this.props.todo.title}
+            </li>
         )
     }
 }
