231e381047ae646c8fbb32d32d006d58702b2a84
Tri Cao <cdtri1619lqdnt@gmail.com>
2023-08-24 10:53:48 +0700

    Tạo Header Component

---
+++ src/components/TodoApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './layouts/Header';
+class TodoApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header></Header>
+            </div>
+        )
+    }
+}
+export default TodoApp;
