735ea6a4bcafea6ff027b03267e0f627396ee8f6
Tri Cao <cdtri1619lqdnt@gmail.com>
2023-08-29 08:57:31 +0700

    Sử dụng destructuring

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 class TodoItem extends React.Component {
     render() {
+        let {id, title, completed} = this.props.todo;
         return (
-            <li className="todo-item" key={this.props.todo.id}>
-                <input type="checkbox" checked={this.props.todo.completed} onChange={() => {this.props.handleCheckbox(this.props.todo.id)}} />
-                <span className={this.props.todo.completed ? "completed" : null}>{this.props.todo.title}</span>
+            <li className="todo-item" key={id}>
+                <input type="checkbox" checked={completed} onChange={() => {this.props.handleCheckbox(id)}} />
+                <span className={completed ? "completed" : null}>{title}</span>
             </li>
         )
     }
