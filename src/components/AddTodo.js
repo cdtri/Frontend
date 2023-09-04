a96faf1f63afe6177ff045c655fbb0b37e6f7727
= <cdtri1619lqdnt@gmail.com>
2023-09-04 14:04:22 +0700

    Cập nhật todo vào danh sách

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -8,9 +8,16 @@ class AddTodo extends React.Component {
             title: e.target.value
         })
     }
+    addTodo = e => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({
+            title: ""
+        })
+    }
     render() {
         return (
-            <form className="form-container">
+            <form className="form-container" onSubmit={this.addTodo}>
                 <input type="text" placeholder="Add Todo..." className="input-text" value={this.state.title} onChange={this.onInputChange} />
                 <input type="submit" value="Submit" className="input-submit" />
             </form>
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,21 +2,22 @@ import React from 'react'
 import Header from './layouts/Header';
 import Todos from './Todos';
 import AddTodo from './AddTodo';
+import {v4 as uuidv4} from 'uuid';
 class TodoApp extends React.Component {
     state = {
         todos: [
             {
-                id: 1,
+                id: uuidv4(),
                 title: "Setup development environment",
                 completed: true
             },
             {
-                id: 2,
+                id: uuidv4(),
                 title: "Develop website and add content",
                 completed: false
             },
             {
-                id: 3,
+                id: uuidv4(),
                 title: "Deploy to live server",
                 completed: false
             }
@@ -39,11 +40,21 @@ class TodoApp extends React.Component {
             })
         })
     }
+    addTodo = title => {
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false
+        }
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
+    }
     render() {
         return (
             <div className='container'>
                 <Header></Header>
-                <AddTodo></AddTodo>
+                <AddTodo addTodo={this.addTodo}></AddTodo>
                 <Todos todos={this.state.todos} handleCheckbox={this.handleCheckboxChange} deleteTodo={this.deleteTodo}></Todos>
             </div>
         )
